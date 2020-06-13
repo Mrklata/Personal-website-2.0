3943128275bffc18523f45d3c671ace1060d99c2
Lukasz <lukasz98kowalczyk@gmail.com>
2020-06-13 14:44:46 +0200

    add files

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lukasz/Desktop/django/personal/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Language(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
